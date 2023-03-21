532b8256250fee8efd3be220ba3fe8fc053dacd5
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-03-20 22:10:32 -0400

    Create lib_example.py

---
+++ lib_example.py
@@ -0,0 +1,9 @@
+import os
+
+my_folder = os.getcwd()
+
+print(f"here are the files in :{my_folder}")
+
+with os.scandir(my_folder) as folder:
+    for entry in folder:
+        print(f" - {entry.name}")
